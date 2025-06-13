eceb1aefba31c33846f6b2740d4195fbfc8a1dc0
Fuad Jemal <fuad47722@gmail.com>
2025-06-14 00:08:25 +0300

    fix: Simplify main.tsx by removing HashRouter and update Vercel configuration to remove unnecessary rewrites

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
